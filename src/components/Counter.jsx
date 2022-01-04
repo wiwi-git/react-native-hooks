d8398333e3178a4b4b315ddbbd0093f6bb654f81
wiwi-git <duse34@hotmail.com>
2022-01-04 19:28:37 +0900

    upload

---
+++ src/components/Counter.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Button from "./Button";
+
+const StyledText = styled.Text`
+    font-size: 24px;
+    margin: 10px;
+`;
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+    return (
+        <>
+            <StyledText>count: {count}</StyledText>
+            <Button
+                title = "-"
+                onPress={()=> {
+                    setCount(count -1);
+                }}
+            ></Button>
+        </>
+    )
+};
+
+export default Counter;
